54a1e080eea43c7a45aebd92c087c7d10d8103f6
Bidhan009 <bidhann999@gmail.com>
2026-01-20 09:48:16 +0530

    connecting with flutter

--- src/dtos/user.dto.ts
+++ src/dtos/user.dto.ts
@@ -1,23 +1,22 @@
 import z from "zod";
 import { UserSchema } from "../types/user.type";
+
 // re-use UserSchema from types
-export const CreateUserDTO = UserSchema.pick(
-    {
-        fullName: true,
-        email: true,
-        password: true
-    }
-).extend( // add new attribute to zod
-    {
-        confirmPassword: z.string().min(6)
-    }
-).refine( // extra validation for confirmPassword
+export const CreateUserDTO = UserSchema.pick({
+    fullName: true,
+    email: true,
+    password: true
+}).extend({
+    confirmPassword: z.string().min(6),
+    phone: z.string().optional() // <-- add this line
+}).refine(
     (data) => data.password === data.confirmPassword,
     {
         message: "Passwords do not match",
         path: ["confirmPassword"]
     }
-)
+);
+
 export type CreateUserDTO = z.infer<typeof CreateUserDTO>;
 
 export const LoginUserDTO = z.object({
