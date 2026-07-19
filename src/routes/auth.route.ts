5da8bf21d36e3278ecb69a7a0e752ba8beaed577
Bidhan009 <bidhann999@gmail.com>
2026-07-19 14:51:06 +0545

    captcha routing

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -3,6 +3,7 @@ import { AuthController } from "../controllers/auth.controller";
 import { authorizationMiddleware } from "../middleware/auth.middleware";
 import { uploads } from "../middleware/upload.middleware";
 import { loginLimiter, registerLimiter } from "../middleware/rate-limit.middleware";
+import { verifyCaptcha } from "../middleware/captcha.middleware";
 
 let authController = new AuthController();
 const router = Router();
@@ -14,6 +15,8 @@ router.get("/whoami", authorizationMiddleware, authController.getProfile);
 router.post("/mfa/setup", authorizationMiddleware, authController.setupMfa);
 router.post("/mfa/confirm", authorizationMiddleware, authController.confirmMfa);
 router.post("/mfa/verify-login", authController.verifyMfa);
+router.post("/register", registerLimiter, verifyCaptcha, authController.register)
+router.post("/login", loginLimiter, verifyCaptcha, authController.login)
 
 router.put(
     '/update-profile',
